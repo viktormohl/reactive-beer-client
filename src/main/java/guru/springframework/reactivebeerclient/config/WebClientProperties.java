06a754162423319075f8ffa295f6fd0e0f66b6e1
viktormohl <viktor.mohl@gmail.com>
2021-06-06 10:39:46 +0200

    add local base url

--- src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
+++ src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
@@ -6,7 +6,8 @@ package guru.springframework.reactivebeerclient.config;
  * create at: 01.06.2021 on 21:19
  */
 public class WebClientProperties {
-    public static final String BASE_URL = "http://api.springframework.guru";
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL_REMOTE = "http://api.springframework.guru";
     public static final String BEER = "api/v1/beer";
     public static final String BEER_BY_ID = "api/v1/beer/{beerId}";
     public static final String BEER_BY_UPC = "api/v1/beerUpc/{upc}";
